1bb4842c9ef63c4ae031cfee302b6a79bbe5962d
ajay-mandaviya <ajay.mandaviya03@gmail.com>
2023-09-10 12:19:25 +0530

    svg animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion'
+import Loader from './Loader';
+
 const btnVarients = {
 
   visible: {
@@ -69,6 +71,7 @@ const Home = () => {
           Create Your Pizza
         </motion.button>
       </Link>
+      <Loader />
     </motion.div>
   )
 }
